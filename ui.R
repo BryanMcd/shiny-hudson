6fb0a04c79c00eb2c4d8143025ba8f6beb56a3db
BryanMcd <bryanmcd92@gmail.com>
2020-06-16 17:01:02 -0700

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,32 @@
+ui <- fluidPage(
+  titlePanel("Hudson 2019 DE"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      h4("DE"),
+      br(),
+      
+      passwordInput("password", "Enter Password:", value = ""),
+      actionButton("go", "Submit"),
+      verbatimTextOutput("value"),
+      
+      br(),
+      selectInput(inputId = "Gene", label = "Enter genes", 
+                  choices = unique(df$gene), 
+                  selected = "Anxa1", 
+                  multiple = TRUE,
+                  selectize = TRUE, 
+                  width = NULL, 
+                  size = NULL),
+
+      
+      submitButton("Update Plot")
+    ),
+    
+    mainPanel(
+      plotOutput("lfc")
+    )
+  )
+)
+
+shinyApp(ui = ui, server = server)
